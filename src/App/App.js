3ee4553cca1da17ab9d2d2db1c570bf98989ee3f
Laura Long <lauralonggone@gmail.com>
2024-04-09 10:46:29 -0600

    Pass props to components

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,60 @@
-import './App.css';
-import React from 'react';
-import Reservations from '../Reservations/Reservations';
-import Form from '../Form/Form';
+import './App.css'
+import React from 'react'
+import Reservations from '../Reservations/Reservations'
+import Form from '../Form/Form'
+import { useState } from 'react'
 
 function App() {
+  // need to GET reservations from the server
+  // write logic to delete a reservation
+
+  const dummyData = [
+    {
+      id: 1,
+      name: 'Christie',
+      date: '12/29',
+      time: '7:00',
+      number: 12,
+    },
+    {
+      id: 2,
+      name: 'Leta',
+      date: '4/5',
+      time: '7:00',
+      number: 2,
+    },
+    {
+      id: 3,
+      name: 'Pam',
+      date: '1/21',
+      time: '6:00',
+      number: 4,
+    },
+  ]
+
+  const [reservations, setReservations] = useState(dummyData)
+
+  const addReservation = (newReservation) => {
+    setReservations([...reservations, newReservation])
+  }
+
+  const deleteReservation = (id) => {
+    const filteredReservations = reservations.filter(
+      (reservation) => reservation.id !== id
+    )
+    setReservations(filteredReservations)
+  }
+
   return (
     <div className="App">
-      <h1 className='app-title'>Turing Cafe Reservations</h1>
-      <Form />
-      <Reservations />
+      <h1 className="app-title">Turing Cafe Reservations</h1>
+      <Form addReservation={addReservation} />
+      <Reservations
+        reservations={reservations}
+        deleteReservation={deleteReservation}
+      />
     </div>
-  );
+  )
 }
 
-export default App; 
+export default App
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Card.css';
 
-const Card = () => {
+const Card = ({id, name, date, time, number, deleteReservation}) => {
     return (
-        <div className="card">
-            <h1 className='name'>Christie</h1>
-            <p className='date'>12/29</p>
-            <p className='time'>7:00</p>
-            <p className='number'>Number of guests: 12</p>
-            <button>Cancel</button>
+        <div className='card'>
+            <h2>{name}</h2>
+            <p>{date}</p>
+            <p>{time} pm</p>
+            <p>Number of guests: {number}</p>
+            <button onClick={() => deleteReservation(id)}>Cancel</button>
         </div>
     );
-};
+}
 
 export default Card;
