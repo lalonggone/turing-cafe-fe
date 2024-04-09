be6037fa99241c12725e54bac4150cbe59cd64a8
Laura Long <lauralonggone@gmail.com>
2024-04-09 11:13:51 -0600

    Begin testing with Cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,34 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('reservation tests', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {
+      statusCode: 200,
+      body: [
+          {
+            id: 1,
+            name: 'Ryan',
+            date: '12/29',
+            time: '7:00',
+            number: 12
+          },
+          {
+            id: 2,
+            name: 'Shannon',
+            date: '4/5',
+            time: '7:00',
+            number: 2
+          }
+        ]
+    })
+  })
+
+  it('displays correct content on initial visit', () => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('reflects form input value', () => {
+  })
+
+  it('adds a new reservation', () => {
+
   })
 })
