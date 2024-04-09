291cf3da266c8e0bd7c9b4a5a773175b084d22e9
Laura Long <lauralonggone@gmail.com>
2024-04-09 10:23:14 -0600

    Track state in form inputs

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Form.css';
 
 const Form = () => {
+    const [name, setName] = useState('')
+    const [date, setDate] = useState('')
+    const [time, setTime] = useState('')
+    const [guests, setGuests] = useState('')
 
+    const handleChange = (event, setState) => {
+        event.preventDefault()
+        setState(event.target.value)
+    }
+    const handleSubmit = (event, setState) => {
+        event.preventDefault()
+        console.log(name, date, time, guests)
+    }
     return (
         <div className='form'>
-            <input type='text' placeholder='Name' />
-            <input type='text' placeholder='Date (mm/dd)' />
-            <input type='text' placeholder='Time' />
-            <input type='text' placeholder='Number of guests' />
-            <button>Make Reservation</button>
+            <input type='text' placeholder='Name' value={name} onChange={(event) => handleChange(event, setName)}/>
+            <input type='text' placeholder='Date (mm/dd)' value={date} onChange={(event) => handleChange(event, setDate)}/>
+            <input type='text' placeholder='Time' value={time} onChange={(event) => handleChange(event, setTime)}/>
+            <input type='text' placeholder='Number of guests' value={guests} onChange={(event) => handleChange(event, setGuests)}/>
+            <button onClick={(event) => handleSubmit(event)}>Make Reservation</button>
         </div>
     );
 };
